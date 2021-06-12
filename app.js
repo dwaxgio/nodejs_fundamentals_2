816de15689e902662ed461d2812ce7c91937e266
edward_gio <edward_gio@hotmail.com>
2021-06-12 18:30:13 -0500

    Express framework, view engine pug, router - methods  and handlers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 1. Including the EXPRESS module
 var express = require("express");
+var pug = require("pug");
 
 var Addition = require("./addition.js");
 var localTutor = require("./nodeTutorial.js");
@@ -8,26 +9,41 @@ var localTutor = require("./nodeTutorial.js");
 var app = express();
 
 // 3. Calling the SET method, to stablish the Jade view engine 
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
-//
+//Default route
 app.get("/", function (req, res) {
-    //res.end("Hello world from Express");
-
-    var result = Addition.AddNumber(1,2); 
-    // console.log(Addition.AddNumber(1,2));
-    console.log(result);
-    //res.end(result);
-
-    // 
-
-    // localTutor.Nodetutorial();
-    // localTutor.Nodetutorial.pTutor();
-
-    var tut = new localTutor.Nodetutorial();
-    tut.pTutor();
     
+
+    // var result = Addition.AddNumber(1,2);     
+    // console.log(result);
+
+    // // 
+    
+    // var tut = new localTutor.Nodetutorial();
+    // tut.pTutor();    
+
+    // //
+
+    //res.send("Hello world from Express, default routhe");
+    
+    res.render("index", {
+        user: "Dwaxgio"
+    });
 });
 
 // 4. Calling the LISTEN method
 var server = app.listen(3000, function () {});
+
+// ROUTERS and HANDLERS ************************
+// Basic structure: app.METHOD(PATH, HANDLER)
+
+// 1. Create the route 1
+app.route("/Route1").get(function(req, res){
+    res.send("Response from the path /Route1");
+});
+
+// 2. Create the route 2
+app.route("/Route2").get(function(req, res){
+    res.send("Response from the path /Route2");
+});
